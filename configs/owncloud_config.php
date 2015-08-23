5531c76ec9898f26ebde5b9b84ad34fc166d2068
Robin Schneider <ypid@riseup.net>
2015-08-23 22:30:16 +0200

    Install additional applications on persistent storage. Closes #22.

--- configs/owncloud_config.php
+++ configs/owncloud_config.php
@@ -5,4 +5,18 @@ $CONFIG = array (
 
     // Memory caching backend configuration: APC user backend
     'memcache.local' => '\OC\Memcache\APCu',
+
+    // Install additional applications on persistent storage.
+    'apps_paths' => array (
+        0 => array (
+            'path'     => OC::$SERVERROOT.'/apps',
+            'url'      => '/apps',
+            'writable' => false,
+        ),
+        1 => array (
+            'path'     => OC::$SERVERROOT.'/apps_persistent',
+            'url'      => '/apps_persistent',
+            'writable' => true,
+        ),
+    ),
 );
